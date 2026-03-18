9aa1ca7601cd8ad83bab1861e2d7a2bbc23ffbb7
Mateo <tsuhikuriislife@gmail.com>
2026-03-18 14:54:06 -0500

    Update algoritmo_busqueda.py

--- algoritmo_busqueda.py
+++ algoritmo_busqueda.py
@@ -1,27 +1,38 @@
-numeros = ["1","4" , "8", "14", "18", "22", "26", "36", "56", "62", "74", "86", "92", "100"]
+import random
 
-inicio = 0
-final = len(numeros)-1
+def numeroEncontrado():
+    print(f"El numero está en la lista en la posicion: {centro}")
+    print(f"Se encontro en {ciclo} ciclos.")
+def ajusteFinal(centro, ciclo, final):
+    final = centro - 1
+    ciclo += 1
+    return final, ciclo
+def ajusteInicio(centro, ciclo, inicio):
+    inicio = centro + 1
+    ciclo += 1
+    return inicio, ciclo
 
+numeros = random.sample(range(0,100), 50)
+numeros.sort()
+print(numeros)
 while True:
-    numeroEscogido = input("Escriba el numero que desea verificar (0-100): ")
+    numeroEscogido = int(input("Escriba el numero que desea verificar (0-100): "))
     if numeroEscogido in numeros:
         ciclo = 0
+        inicio = 0
+        final = len(numeros)-1
         while True:
             centro = (inicio+final)//2
-            if numeroEscogido < numeros[centro]:
-                final = centro
-                ciclo += 1
+            if numeroEscogido == numeros[centro]:
+                numeroEncontrado()
+                break
+            elif numeroEscogido < numeros[centro]:
+                final, ciclo = ajusteFinal(centro, ciclo, final)
                 print(f"{numeroEscogido} es menor a {numeros[centro]}")
                 continue
-            elif numeroEscogido > numeros[centro]:
-                inicio = centro
-                ciclo += 1
+            else:
+                inicio, ciclo = ajusteInicio(centro, ciclo, inicio)
                 print(f"{numeroEscogido} es mayor a {numeros[centro]}")
                 continue
-            elif numeroEscogido == numeros[centro]:
-                print(f"El numero está en la lista en la pocicion: {centro}")
-                print(f"Se encontro en {ciclo} ciclos.")
-                break
-    else:
+    else:            
         print("El numero no esta en la lista.")
